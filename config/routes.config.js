3b1610128ec9a4d0536c9f2a654a94ee88f89d36
acanest83 <133660942+acanest83@users.noreply.github.com>
2023-09-02 08:57:12 +0200

    Register views

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,8 +4,8 @@ const users = require("../controllers/users.controller");
 const spotify = require("../controllers/spotify.controller");
 
 
-// router.get("/register", users.register);
-// router.post("/register", users.doRegister);
+router.get("/register", users.register);
+router.post("/register", users.doRegister);
 // router.get('/login', users.login);
 // router.post('/login', users.doLogin);
 // router.get('/profile', secure.isAuthenticated, users.profile);
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,3 +4,18 @@ const mongoose = require("mongoose");
 module.exports.home = (req, res) => {
   res.render("home", {});
 };
+
+module.exports.register = (req, res, next) => res.render("users/register"); 
+
+module.exports.doRegister = (req, res, next) => {
+  User.create(req.body) 
+    .then((user) => res.redirect("users/register"))
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.render("users/register", { user: req.body, errors: error.errors }) 
+        
+      } else {
+        next(error); 
+      }
+    })
+}
