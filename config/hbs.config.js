4a151eb5161131712e869d1b3c43c903e61047d2
Naiim Taefy <naiim.taefy.company@gmail.com>
2023-09-04 12:43:41 +0200

    Home carusel Fix

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -5,11 +5,3 @@ hbs.registerPartials(`${__dirname}/../views/partials`);
 hbs.registerHelper("prettyDate", (date) => {
   return date.toLocaleDateString("es-ES", {});
 });
-
-hbs.registerHelper("#myCarousel", () => {
-  const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: 2000,
-    touch: false,
-  });
-  return carousel;
-});
